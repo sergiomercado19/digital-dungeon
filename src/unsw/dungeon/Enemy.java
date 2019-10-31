81a405b2af8e7e6e1f5e4d78afb7f25f653d42d2
Sergio Mercado Ruiz <marcelo.mercado.1999@gmail.com>
2019-11-01 10:34:34 +1100

    Enemy implements basic movement strategy

---
+++ src/unsw/dungeon/Enemy.java
@@ -0,0 +1,31 @@
+package unsw.dungeon;
+
+import java.lang.*;
+
+public class Enemy extends Entity {
+
+   private Player player;
+   
+   public Enemy(int x, int y, Player player) {
+      super(x, y);
+      this.player = player;
+   }
+   
+   public void moveTowardsPlayer() {
+      int xDist = Math.abs(this.getX()-this.player.getX());
+      int yDist = Math.abs(this.getY()-this.player.getY());
+      
+      if (xDist > yDist) {
+         if (this.player.getX() < this.getX()) makeMove(Direction.LEFT);
+         else makeMove(Direction.RIGHT);
+      } else {
+         if (this.player.getY() < this.getY()) makeMove(Direction.UP);
+         else makeMove(Direction.DOWN);
+      }
+   }
+   
+   public void makeMove(Direction dir) {
+      
+   }
+   
+}
