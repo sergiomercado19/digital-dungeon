47b2cc029773f4efcef2fe240cdd09b2f59c11d4
nutmeg-5000 <z5208738@student.unsw.edu.au>
2019-10-31 15:00:40 +1100

    added door and key observer

--- src/unsw/dungeon/Key.java
+++ src/unsw/dungeon/Key.java
@@ -1,11 +1,16 @@
 package unsw.dungeon;
 
-public class Key implements Item {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Key implements Item, KeySubject {
 	
 	public int ID;
+	public List<KeyObserver> observers;
 	
 	public Key(int ID) {
 		this.ID = ID;
+		ArrayList<KeyObserver> observers = new ArrayList<>();
 	}
 
 	@Override
@@ -19,6 +24,26 @@ public class Key implements Item {
 		// TODO Auto-generated method stub
 		player.addKey(ID);
 		// delete item
+	}
+
+	@Override
+	public void addObserver(KeyObserver o) {
+		observers.add(o);
+	}
+
+	@Override
+	public void removeObserver(KeyObserver o) {
+		int i = observers.indexOf(o);
+		if (i >= 0) {
+			observers.remove(i);
+		}
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (KeyObserver o : observers) {
+			o.update();
+		}
 	}
 
 }
---
+++ src/unsw/dungeon/KeyObserver.java
@@ -0,0 +1,5 @@
+package unsw.dungeon;
+
+public interface KeyObserver {
+	public void update();
+}
