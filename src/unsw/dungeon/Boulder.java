42916065a744f1716eb82e5fac2c7cc7f7abac2f
nutmeg-5000 <z5208738@student.unsw.edu.au>
2019-11-03 12:44:36 +1100

    began javadoc

--- src/unsw/dungeon/Boulder.java
+++ src/unsw/dungeon/Boulder.java
@@ -1,16 +1,31 @@
 package unsw.dungeon;
 
+/**
+ * a boulder, which can be pushed by a player to trigger a floor switch
+ * @author Rory
+ *
+ */
 public class Boulder extends MovableEntity {
 	
    private Dungeon dungeon;
    private FloorSwitch floorSwitch;
 
+   /**
+    * create a new boulder
+    * @param dungeon the dungeon of the boulder
+    * @param x x position of boulder
+    * @param y y position of boulder
+    */
 	public Boulder(Dungeon dungeon, int x, int y) {
 		super(dungeon, x, y, false);
 		this.dungeon = dungeon;
 		this.floorSwitch = null;
 	}
 	
+	/**
+	 * push the boulder in a direction
+	 * @param d the direction to push
+	 */
 	public void push(Direction d) {
 		// FIXME
 		// check
@@ -18,15 +33,25 @@ public class Boulder extends MovableEntity {
 		makeMove(d);
 	}
 
+	/**
+	 * get the dungeon the boulder is contained in
+	 */
 	public Dungeon getDungeon() {
 		return dungeon;
 	}
 
+	/**
+	 * trigger a floor switch when the boulder is on top
+	 * @param s
+	 */
    public void activateSwitch(FloorSwitch s) {
       this.floorSwitch = s;
       this.floorSwitch.activate();
    }
 
+   /**
+    * untrigger a floor switch when a boulder is moved off it
+    */
    public void deactivateSwitch() {
       if (this.floorSwitch != null) {         
          this.floorSwitch.deactivate();
@@ -34,6 +59,9 @@ public class Boulder extends MovableEntity {
       }
    }
 
+   /**
+    * when the boulder is moved, the floor switch is also untriggered
+    */
    @Override
    public void makeMove(Direction d) {
       // Deactivate floorSwitch before moving
--- src/unsw/dungeon/Door.java
+++ src/unsw/dungeon/Door.java
@@ -1,22 +1,37 @@
 package unsw.dungeon;
 
+/**
+ * a door, which can be opened with a key
+ * @author Rory
+ *
+ */
 public class Door extends InertEntity {
 	
-//	private boolean isOpen;
 	private int ID;
 
+	/**\
+	 * create a new door
+	 * @param x x position of the door
+	 * @param y y position of the door
+	 * @param ID the id of the key that can open the door
+	 */
 	public Door(int x, int y, int ID) {
 		super(x, y, true);
-//		isOpen = false;
 		this.ID = ID;
 	}
 
+	/**
+	 * get the id of the door
+	 * @return the id of the door
+	 */
 	public int getID() {
 		return ID;
 	}
 
+	/**
+	 * set the door to an open state so players can pass through
+	 */
 	public void unlock() {
-//		isOpen = true;
 		super.setSolid(false);
 	}
 }
