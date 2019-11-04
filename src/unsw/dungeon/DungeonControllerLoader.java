0a427128b539b659d2747f2e74b0634de98e13ce
nutmeg-5000 <z5208738@student.unsw.edu.au>
2019-11-04 15:32:40 +1100

    begun linking to frontend

--- src/unsw/dungeon/DungeonControllerLoader.java
+++ src/unsw/dungeon/DungeonControllerLoader.java
@@ -19,75 +19,135 @@ import javafx.scene.layout.GridPane;
  */
 public class DungeonControllerLoader extends DungeonLoader {
 
-   private List<ImageView> entities;
+	private List<ImageView> entities;
 
-   //Images
-   private Image playerImage;
-   private Image wallImage;
+	//Images
+	private Image playerImage;
+	private Image wallImage;
+	private Image boulderImage;
+	private Image treasureImage;
+	private Image keyImage;
+	private Image doorImage;
+	private Image swordImage;
+	private Image invincibilityImage;
+	private Image portalImage;
+	private Image enemyImage;
+	private Image floorSwitchImage;
+	private Image exitImage;
 
-   public DungeonControllerLoader(String filename)
-         throws FileNotFoundException {
-      super(filename);
-      entities = new ArrayList<>();
-      playerImage = new Image("/human_new.png");
-      wallImage = new Image("/brick_brown_0.png");
-   }
+	public DungeonControllerLoader(String filename)
+			throws FileNotFoundException {
+		super(filename);
+		entities = new ArrayList<>();
+		playerImage = new Image("/human_new.png");
+		wallImage = new Image("/brick_brown_0.png");
+		boulderImage = new Image("/boulder.png");
+		treasureImage = new Image("/gold_pile.png");
+		keyImage = new Image("/key.png");
+		doorImage = new Image("/closed_door.png");
+		swordImage = new Image("/greatsword_1_new.png");
+		invincibilityImage = new Image("/brilliant_blue_new.png");
+		portalImage = new Image("/portal.png");
+		enemyImage = new Image("/deep_elf_master_archer.png");
+		floorSwitchImage = new Image("/pressure_plate.png");
+		exitImage = new Image("exit.png");
+	}
 
-   //@Override
-   public void onLoad(Entity player) {
-      ImageView view = new ImageView(playerImage);
-      addEntity(player, view);
-   }
+	@Override
+	public void onLoad(Player player) {
+		ImageView view = new ImageView(playerImage);
+		addEntity(player, view);
+	}
 
-   //@Override
-   public void onLoad(Wall wall) {
-      ImageView view = new ImageView(wallImage);
-      addEntity(wall, view);
-   }
+	@Override
+	public void onLoad(Wall wall) {
+		ImageView view = new ImageView(wallImage);
+		addEntity(wall, view);
+	}
+	
+	@Override
+	protected void onLoad(Enemy enemy) {
+		ImageView view = new ImageView(enemyImage);
+		addEntity(enemy, view);
+	}
 
-   private void addEntity(Entity entity, ImageView view) {
-      trackPosition(entity, view);
-      entities.add(view);
-   }
+	@Override
+	protected void onLoad(Portal portal) {
+		ImageView view = new ImageView(portalImage);
+		addEntity(portal, view);
+	}
 
-   /**
-    * Set a node in a GridPane to have its position track the position of an
-    * entity in the dungeon.
-    *
-    * By connecting the model with the view in this way, the model requires no
-    * knowledge of the view and changes to the position of entities in the
-    * model will automatically be reflected in the view.
-    * @param entity
-    * @param node
-    */
-   private void trackPosition(Entity entity, Node node) {
-      GridPane.setColumnIndex(node, entity.getX());
-      GridPane.setRowIndex(node, entity.getY());
-      entity.x().addListener(new ChangeListener<Number>() {
-         @Override
-         public void changed(ObservableValue<? extends Number> observable,
-               Number oldValue, Number newValue) {
-            GridPane.setColumnIndex(node, newValue.intValue());
-         }
-      });
-      entity.y().addListener(new ChangeListener<Number>() {
-         @Override
-         public void changed(ObservableValue<? extends Number> observable,
-               Number oldValue, Number newValue) {
-            GridPane.setRowIndex(node, newValue.intValue());
-         }
-      });
-   }
+	@Override
+	protected void onLoad(FloorSwitch floorSwitch) {
+		ImageView view = new ImageView(floorSwitchImage);
+		addEntity(floorSwitch, view);
+	}
 
-   /**
-    * Create a controller that can be attached to the DungeonView with all the
-    * loaded entities.
-    * @return
-    * @throws FileNotFoundException
-    */
-   public DungeonController loadController() throws FileNotFoundException {
-      return new DungeonController(load(), entities);
-   }
+	@Override
+	protected void onLoad(Boulder boulder) {
+		ImageView view = new ImageView(boulderImage);
+		addEntity(boulder, view);
+	}
 
+	@Override
+	protected void onLoad(Door door) {
+		ImageView view = new ImageView(doorImage);
+		addEntity(door, view);
+	}
 
+	@Override
+	protected void onLoad(Collectable c) {
+		ImageView view = new ImageView(treasureImage);
+		addEntity(c, view);
+	}
+
+	@Override
+	protected void onLoad(Exit exit) {
+		ImageView view = new ImageView(exitImage);
+		addEntity(exit, view);
+	}
+
+	private void addEntity(Entity entity, ImageView view) {
+		trackPosition(entity, view);
+		entities.add(view);
+	}
+
+	/**
+	 * Set a node in a GridPane to have its position track the position of an
+	 * entity in the dungeon.
+	 *
+	 * By connecting the model with the view in this way, the model requires no
+	 * knowledge of the view and changes to the position of entities in the
+	 * model will automatically be reflected in the view.
+	 * @param entity
+	 * @param node
+	 */
+	private void trackPosition(Entity entity, Node node) {
+		GridPane.setColumnIndex(node, entity.getX());
+		GridPane.setRowIndex(node, entity.getY());
+		entity.x().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+					Number oldValue, Number newValue) {
+				GridPane.setColumnIndex(node, newValue.intValue());
+			}
+		});
+		entity.y().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+					Number oldValue, Number newValue) {
+				GridPane.setRowIndex(node, newValue.intValue());
+			}
+		});
+	}
+
+	/**
+	 * Create a controller that can be attached to the DungeonView with all the
+	 * loaded entities.
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	public DungeonController loadController() throws FileNotFoundException {
+		return new DungeonController(load(), entities);
+	}
 }
--- src/unsw/dungeon/DungeonLoader.java
+++ src/unsw/dungeon/DungeonLoader.java
@@ -20,180 +20,196 @@ import org.json.JSONTokener;
  * For milestone 3 we will change this to an abstract class, extended by 
  * an EntityLoader that will render the visual elements of the dungeon.
  */
-public class DungeonLoader {
+public abstract class DungeonLoader {
 
-   private JSONObject json;
+	private JSONObject json;
 
-   public DungeonLoader(String filename) throws FileNotFoundException {
-      json = new JSONObject(new JSONTokener(new FileReader("dungeons/" + filename)));
-   }
+	public DungeonLoader(String filename) throws FileNotFoundException {
+		json = new JSONObject(new JSONTokener(new FileReader("dungeons/" + filename)));
+	}
 
-   /**
-    * Parses the JSON to create a dungeon.
-    * @return the newly created dungeon
-    */
-   public Dungeon load() {
-      int width = json.getInt("width");
-      int height = json.getInt("height");
+	/**
+	 * Parses the JSON to create a dungeon.
+	 * @return the newly created dungeon
+	 */
+	public Dungeon load() {
+		int width = json.getInt("width");
+		int height = json.getInt("height");
 
-      // 1. Create dungeon
-      Dungeon dungeon = new Dungeon(width, height);
+		// 1. Create dungeon
+		Dungeon dungeon = new Dungeon(width, height);
 
-      JSONArray jsonEntities = json.getJSONArray("entities");
+		JSONArray jsonEntities = json.getJSONArray("entities");
 
-      // 2. Load entities
-      for (int i = 0; i < jsonEntities.length(); i++) {
-         loadEntity(dungeon, jsonEntities.getJSONObject(i));
-      }
+		// 2. Load entities
+		for (int i = 0; i < jsonEntities.length(); i++) {
+			loadEntity(dungeon, jsonEntities.getJSONObject(i));
+		}
 
-      // 3. Alert enemies of players
-      dungeon.alertEnemies();
+		// 3. Alert enemies of players
+		dungeon.alertEnemies();
 
-      // 4. Link portals
-      dungeon.linkPortals();
+		// 4. Link portals
+		dungeon.linkPortals();
 
-      // 5. Set dungeon goals
-      dungeon.setGoals(loadGoal(dungeon, json.getJSONObject("goal-condition")));
+		// 5. Set dungeon goals
+		dungeon.setGoals(loadGoal(dungeon, json.getJSONObject("goal-condition")));
 
-      // 6. Start dungeon
-      dungeon.setState(DungeonState.INPROGRESS);
+		// 6. Start dungeon
+		dungeon.setState(DungeonState.INPROGRESS);
 
-      return dungeon;
-   }
+		return dungeon;
+	}
 
-   /**
-    * load a goal from json into a dungeon
-    * @param dungeon the dungeon to load the goal into
-    * @param json the supplied json
-    * @return the newly created goal component
-    */
-   private GoalComponent loadGoal(Dungeon dungeon, JSONObject json) {
-      String name = json.getString("goal");
-      GoalComponent gc;
+	/**
+	 * load a goal from json into a dungeon
+	 * @param dungeon the dungeon to load the goal into
+	 * @param json the supplied json
+	 * @return the newly created goal component
+	 */
+	private GoalComponent loadGoal(Dungeon dungeon, JSONObject json) {
+		String name = json.getString("goal");
+		GoalComponent gc;
 
-      if (name.equals("AND") || name.equals("OR")) {
+		if (name.equals("AND") || name.equals("OR")) {
 
-         ArrayList<GoalComponent> subgoals = new ArrayList<GoalComponent>();
-         JSONArray jsonSubgoals = json.getJSONArray("subgoals");
-         for (int i = 0; i < jsonSubgoals.length(); i++) {
-            subgoals.add(loadGoal(dungeon, jsonSubgoals.getJSONObject(i)));
-         }
+			ArrayList<GoalComponent> subgoals = new ArrayList<GoalComponent>();
+			JSONArray jsonSubgoals = json.getJSONArray("subgoals");
+			for (int i = 0; i < jsonSubgoals.length(); i++) {
+				subgoals.add(loadGoal(dungeon, jsonSubgoals.getJSONObject(i)));
+			}
 
-         if (name.equals("AND")) gc = new GoalAnd(subgoals);
-         else gc = new GoalOr(subgoals);
+			if (name.equals("AND")) gc = new GoalAnd(subgoals);
+			else gc = new GoalOr(subgoals);
 
-      } else {
+		} else {
 
-         ArrayList<Entity> entities = new ArrayList<Entity>();
+			ArrayList<Entity> entities = new ArrayList<Entity>();
 
-         switch (name) {
-         case "exit":
-            // Get goal relevant entity list
-            entities.addAll(dungeon.getEntityArrayList("exit"));
-            break;
-         case "enemies":
-            // Get goal relevant entity list
-            entities.addAll(dungeon.getEntityArrayList("enemy"));
-            break;
-         case "boulders":
-            // Get goal relevant entity list
-            entities.addAll(dungeon.getEntityArrayList("boulder"));
-            break;
-         case "treasure":
-            // Get goal relevant entity list
-            entities.addAll(dungeon.getEntityArrayList("treasure"));
-            break;
-         }
+			switch (name) {
+			case "exit":
+				// Get goal relevant entity list
+				entities.addAll(dungeon.getEntityArrayList("exit"));
+				break;
+			case "enemies":
+				// Get goal relevant entity list
+				entities.addAll(dungeon.getEntityArrayList("enemy"));
+				break;
+			case "boulders":
+				// Get goal relevant entity list
+				entities.addAll(dungeon.getEntityArrayList("boulder"));
+				break;
+			case "treasure":
+				// Get goal relevant entity list
+				entities.addAll(dungeon.getEntityArrayList("treasure"));
+				break;
+			}
 
-         gc = new Goal(name, entities.size());
-         // Pass this Goal (that implements GoalObserver) into all the relevant entities (that implement GoalSubject)
-         for (Entity e : entities) {
-            ((GoalSubject) e).addObserver((GoalObserver) gc);
-         }
+			gc = new Goal(name, entities.size());
+			// Pass this Goal (that implements GoalObserver) into all the relevant entities (that implement GoalSubject)
+			for (Entity e : entities) {
+				((GoalSubject) e).addObserver((GoalObserver) gc);
+			}
 
-      }
+		}
 
-      return gc;
-   }
+		return gc;
+	}
 
-   /**
-    * load an entity from json into a dungeon
-    * @param dungeon the dungeon to load the entity into
-    * @param json the supplied json
-    */
-   private void loadEntity(Dungeon dungeon, JSONObject json) {
-      String type = json.getString("type");
-      int x = json.getInt("x");
-      int y = json.getInt("y");
+	/**
+	 * load an entity from json into a dungeon
+	 * @param dungeon the dungeon to load the entity into
+	 * @param json the supplied json
+	 */
+	private void loadEntity(Dungeon dungeon, JSONObject json) {
+		String type = json.getString("type");
+		int x = json.getInt("x");
+		int y = json.getInt("y");
 
-      Entity entity = null;
-      switch (type) {
-      case "player":
-         Player player = new Player(dungeon, x, y);
-         dungeon.setPlayer(player);
-         //            onLoad(player);
-         entity = player;
-         break;
-      case "wall":
-         Wall wall = new Wall(x, y);
-         //            onLoad(wall);
-         entity = wall;
-         break;
-      case "exit":
-         Exit exit = new Exit(x, y);
-         entity = exit;
-         break;
-      case "treasure":
-         Treasure treasure = new Treasure();
-         Collectable cTreasure = new Collectable(x, y, treasure);
-         entity = cTreasure;
-         break;
-      case "door":
-         int doorID = json.getInt("id");;
-         Door door = new Door(x, y, doorID);
-         entity = door;
-         break;
-      case "key":
-         int keyID = json.getInt("id");;
-         Key key = new Key(keyID);
-         Collectable cKey = new Collectable(x, y, key);
-         entity = cKey;
-         break;
-      case "boulder":
-         Boulder boulder = new Boulder(dungeon, x, y);
-         entity = boulder;
-         break;
-      case "floorSwitch":
-         FloorSwitch floorSwitch = new FloorSwitch(x, y);
-         entity = floorSwitch;
-         break;
-      case "portal":
-         int portalID = json.getInt("id");
-         Portal portal = new Portal(x, y, portalID);
-         entity = portal;
-         break;
-      case "enemy":
-         Enemy enemy = new Enemy(dungeon, x, y);
-         entity = enemy;
-         break;
-      case "sword":
-         Sword sword = new Sword();
-         Collectable cSword = new Collectable(x, y, sword);
-         entity = cSword;
-         break;
-      case "invincibility":
-         Invincibility invincibility = new Invincibility();
-         Collectable cInvincibility = new Collectable(x, y, invincibility);
-         entity = cInvincibility;
-         break;
-      }
-      dungeon.addEntity(entity);
-   }
+		Entity entity = null;
+		switch (type) {
+		case "player":
+			Player player = new Player(dungeon, x, y);
+			dungeon.setPlayer(player);
+			onLoad(player);
+			entity = player;
+			break;
+		case "wall":
+			Wall wall = new Wall(x, y);
+			onLoad(wall);
+			entity = wall;
+			break;
+		case "exit":
+			Exit exit = new Exit(x, y);
+			onLoad(exit);
+			entity = exit;
+			break;
+		case "treasure":
+			Treasure treasure = new Treasure();
+			Collectable cTreasure = new Collectable(x, y, treasure);
+			onLoad(cTreasure);
+			entity = cTreasure;
+			break;
+		case "door":
+			int doorID = json.getInt("id");;
+			Door door = new Door(x, y, doorID);
+			onLoad(door);
+			entity = door;
+			break;
+		case "key":
+			int keyID = json.getInt("id");;
+			Key key = new Key(keyID);
+			Collectable cKey = new Collectable(x, y, key);
+			onLoad(cKey);
+			entity = cKey;
+			break;
+		case "boulder":
+			Boulder boulder = new Boulder(dungeon, x, y);
+			onLoad(boulder);
+			entity = boulder;
+			break;
+		case "floorSwitch":
+			FloorSwitch floorSwitch = new FloorSwitch(x, y);
+			onLoad(floorSwitch);
+			entity = floorSwitch;
+			break;
+		case "portal":
+			int portalID = json.getInt("id");
+			Portal portal = new Portal(x, y, portalID);
+			onLoad(portal);
+			entity = portal;
+			break;
+		case "enemy":
+			Enemy enemy = new Enemy(dungeon, x, y);
+			onLoad(enemy);
+			entity = enemy;
+			break;
+		case "sword":
+			Sword sword = new Sword();
+			Collectable cSword = new Collectable(x, y, sword);
+			onLoad(cSword);
+			entity = cSword;
+			break;
+		case "invincibility":
+			Invincibility invincibility = new Invincibility();
+			Collectable cInvincibility = new Collectable(x, y, invincibility);
+			onLoad(cInvincibility);
+			entity = cInvincibility;
+			break;
+		}
+		dungeon.addEntity(entity);
+	}
 
-   //   public abstract void onLoad(Entity player);
-   //
-   //   public abstract void onLoad(Wall wall);
+	protected abstract void onLoad(Enemy enemy);
+	protected abstract void onLoad(Portal portal);
+	protected abstract void onLoad(FloorSwitch floorSwitch);
+	protected abstract void onLoad(Boulder boulder);
+	protected abstract void onLoad(Door door);
+	protected abstract void onLoad(Collectable cTreasure);
+	protected abstract void onLoad(Exit exit);
+	public abstract void onLoad(Player player);
+	public abstract void onLoad(Wall wall);
 
-   // TODO MILESTONE 3: Create additional abstract methods for the other entities
+	// TODO MILESTONE 3: Create additional abstract methods for the other entities
 
 }
