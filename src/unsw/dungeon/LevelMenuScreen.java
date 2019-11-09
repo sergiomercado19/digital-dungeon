ba1d5b64753a82183d8fcb75c02cac771ae06d8a
Sergio Mercado Ruiz <marcelo.mercado.1999@gmail.com>
2019-11-09 11:11:33 +1100

    Started working on Level Menu, broke down the responsibility of DungeonApplication

--- src/unsw/dungeon/LevelMenuScreen.java
+++ src/unsw/dungeon/LevelMenuScreen.java
@@ -7,37 +7,37 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainMenuScreen {
+public class LevelMenuScreen {
    private Stage stage;
    private String title;
-   private MainMenuController controller;
+   private LevelMenuController controller;
 
    private Scene scene;
 
-   public MainMenuScreen(Stage stage) throws IOException {
+   public LevelMenuScreen(Stage stage) throws IOException {
       this.stage = stage;
       title = "Digital Dungeon";
 
-      controller = new MainMenuController();
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+      controller = new LevelMenuController();
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("LevelMenu.fxml"));
       loader.setController(controller);
 
       // load into a Parent node called root
       Parent root = loader.load();
       scene = new Scene(root, 800, 480);
    }
-
-   public void setLevelScreen(LevelMenuScreen levelScreen) {
-      controller.setLevelScreen(levelScreen);
-   }
    
+   public void setMainScreen(MainMenuScreen mainScreen) {
+      controller.setMainScreen(mainScreen);
+   }
+
    public void start() {
       stage.setTitle(title);
       stage.setScene(scene);
       stage.show();
    }
 
-   public MainMenuController getController() {
+   public LevelMenuController getController() {
       return controller;
    }
 }
