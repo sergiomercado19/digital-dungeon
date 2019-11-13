413b333fb8453858ee3bc4fbc1d3fff8bcfcfbb3
Sergio Mercado Ruiz <marcelo.mercado.1999@gmail.com>
2019-11-13 11:00:57 +1100

    Added sounds effects for the whole game

--- src/unsw/dungeon/DungeonApplication.java
+++ src/unsw/dungeon/DungeonApplication.java
@@ -3,21 +3,29 @@ package unsw.dungeon;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class DungeonApplication extends Application {
 
    @Override
    public void start(Stage primaryStage) throws IOException {
-      
+
       // Initialize menu pages
       MainMenuScreen menuScreen = new MainMenuScreen(primaryStage);
       LevelMenuScreen levelScreen = new LevelMenuScreen(primaryStage);
-      
+
       // Link menu pages
       menuScreen.setLevelScreen(levelScreen);
       levelScreen.setMainScreen(menuScreen);
-      
+
+      // Play music
+      AudioClip audio = new AudioClip(getClass().getResource("/background.wav").toExternalForm());
+      audio.setVolume(0.3f);
+      audio.setCycleCount(MediaPlayer.INDEFINITE);
+      audio.play();
+
       menuScreen.start();
 
    }
